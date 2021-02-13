87faa7ebebdc99dbe80667a58b0e4a695c0d809a
LucaLombardo <35398703+LucaLombardo@users.noreply.github.com>
2021-02-13 21:11:46 +0100

    Add files via upload

---
+++ WorkAdventureTest-master/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset1" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <image source="../workadventure-map-tcm-master/paris/tileset1.png" width="352" height="352"/>
+</tileset>
